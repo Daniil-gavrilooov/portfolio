dc63ed2dfc8d134406c2de01ded613ff64e89c85
Daniil-gavrilooov <85822019+Daniil-gavrilooov@users.noreply.github.com>
2022-08-31 09:53:54 +0300

    add branch slider, add js slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,3 @@
-
-
-
 let mailCopyText = document.querySelector('#sidebar__mail-copy')
 let mailBtn = document.querySelector('.sidebar__item-mail')
 
@@ -16,11 +13,70 @@ mailBtn.onclick = function () {
     setTimeout(removeText, 4000)
 }
 
-
-
 mailBtn.addEventListener("copy", (event) => {
     event.preventDefault();
     if (event.clipboardData) {
         event.clipboardData.setData("text/plain", mailCopyText.textContent);
     }
 });
+
+
+$(document).ready(function() {
+    let position = 0;
+    const slidesToShow = 6;
+    const slidesToScroll = 1;
+    const container = $('.content__container');
+    const track = $('.content__track');
+    const item = $('.content__item');
+    const btnPrev = $('.prev__btn');
+    const btnNext = $('.next__btn');
+    const itemsCount = item.length;
+    const itemWidth = container.width() / slidesToShow;
+    const movePosition = slidesToScroll * itemWidth;
+
+
+    item.each(function(index, item){
+        $(item).css({
+            minWidth: itemWidth - 18,
+        })
+    });
+
+    btnNext.click(function() {
+        const itemsLeft = itemsCount - (Math.abs(position) + slidesToShow *itemWidth) / itemWidth;
+
+        position -= itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
+
+        setPosition()
+        checkBtns()
+    });
+
+    btnPrev.click(function() {
+        const itemsLeft = Math.abs(position) / itemWidth;
+
+        position +=  itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
+
+        setPosition()
+        checkBtns()
+
+    })
+
+    const setPosition = () => {
+        track.css({
+            transform: `translateX(${position}px)`,
+        });
+    }
+
+    const checkBtns = () => {
+        btnPrev.prop('disabled', position === 0);
+        btnNext.prop(
+            'disabled',
+            position <= -(itemsCount - slidesToShow) * itemWidth
+        )
+    }
+
+    checkBtns()
+
+
+
+
+})
